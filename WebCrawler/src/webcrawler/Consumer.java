74ceedea8c24edca562ac13bf20fc8840113edf4
Teofebano <teofebano.stei@gmail.com>
2015-12-16 14:54:21 +0700

    second commit

--- WebCrawler/src/webcrawler/Consumer.java
+++ WebCrawler/src/webcrawler/Consumer.java
@@ -7,8 +7,13 @@ package webcrawler;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.util.Pair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -19,32 +24,52 @@ import org.jsoup.select.Elements;
  * @author user
  */
 public class Consumer {
-    private String url = "http://www.jsoup.org/";
-    private int depth = 1;
+    private String url = "http://jsoup.org";
+    private int depth = 2;
+    PriorityQueue<String> anchors = new PriorityQueue<String>();
+    
     public void Start(){
+        anchors.add(url);
         Crawl();
     }
     
     private void Crawl(){
-        
         String url = this.getURL();
-        HashSet<String> anchors = new HashSet<String>();
         try {
             Document doc;
-            doc = Jsoup.connect(url).get();
-            Elements links = doc.select("a");
-            for (Element link : links){
-                String anchor = link.attr("href").trim();
-                anchors.add(anchor);
-                System.out.println(anchor);
+            Vector<Pair<Elements,String>> links = new Vector<Pair<Elements,String>>();
+            for (int i=0;i<depth;i++){
+                String domain = "";
+                for (int j=0;j<anchors.size();i++){
+                    System.out.println(anchors.element());
+                    doc = Jsoup.connect(anchors.element()).get();
+                    Elements E = doc.select("a");
+//                    System.out.println(E);
+                    domain = anchors.element();
+                    Pair pair = new Pair(E,domain);
+                    links.add(pair);
+                    
+                    anchors.remove();
+                }
+                System.out.println("--------------------");
+//                System.out.println(links.size());
+                for (int j=0;j<links.size();j++){
+                    Elements ListE = links.elementAt(j).getKey();
+                    String domainE = links.elementAt(j).getValue();
+                    for (Element link:ListE){
+                        String anchor = link.attr("href").trim();
+                        if (anchor.startsWith("/")){
+                            anchor = domainE + anchor;
+                        }
+                        anchors.add(anchor);
+                        System.out.println(anchor);
+                    }
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(WebCrawler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("-------");
-        for (String s:anchors){
-            System.out.println(s);
-        }
+        
     }
     
     private String getURL(){
